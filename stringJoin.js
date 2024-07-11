3eb24011cbe431e578d1197f9be28a2429ddbe2b
Tara Patterson <tarapatterson01@gmail.com>
2024-07-11 20:24:50 +0200

    JoinString

---
+++ stringJoin.js
@@ -0,0 +1,9 @@
+function sentencify(words) {
+
+    const firstWord = words[0].charAt(0).toUpperCase() + words[0].slice(1);
+    const sentence = words.slice(1).reduce((sentence, word) => sentence + " " + word, firstWord);
+    return sentence + ".";
+}
+
+const words = ['this', 'is', 'a', 'sentence'];
+console.log(sentencify(words)); 
