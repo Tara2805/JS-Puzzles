55b69a970f992c5b4b8d9c886e6378e341cb6c26
Tara Patterson <tarapatterson01@gmail.com>
2024-07-11 21:39:03 +0200

    Jenny's secret message

---
+++ secretMessage.js
@@ -0,0 +1,14 @@
+// 8 kyu Jenny's secret message
+
+function greet(name){
+  
+  if(name === "Johnny")
+    return "Hello, my love!";
+  else
+  {
+    return "Hello, " + name + "!";
+  }
+  
+}
+
+console.log(greet("Johnny"))
